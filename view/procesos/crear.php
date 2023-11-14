34f103f57f742873995b241504181b656f74a81e
IkerCG2003 <100007043.joan23@fje.edu>
2023-11-14 20:47:31 +0100

    Validación repetición correo electrónico y número de teléfono - 14.11.2023

--- view/procesos/crear.php
+++ view/procesos/crear.php
@@ -1,31 +1,84 @@
 <?php
-try {
+try 
+{
+    // Establecemos una cadena vacía para almacenar los mensajes de error.
+    $errores = "";
     // Recogida de datos
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $email = $_POST["email"];
     $pwd = $_POST["pwd"];
+    $pwdEncript = hash("sha256", $pwd);
     $telefono = $_POST["telefono"];
     $curso = $_POST["curso"];
-echo $curso."<br>";
     include_once("./conexion.php");
 
     mysqli_autocommit($conn, false);
     mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_WRITE);
-    // Definimos la consulta
-    $stmt = mysqli_stmt_init($conn);
-    $sql = "INSERT INTO `tbl_alumnos` (`id`, `nombre`, `apellido`, `email`, `pass`, `telefono`, `id_curso`) VALUES (NULL, ?, ?, ?, ?, ?, ?)";
-    mysqli_stmt_prepare($stmt, $sql);
-    mysqli_stmt_bind_param($stmt, "sssssi", $nombre, $apellido, $email, $pwd, $telefono, $curso);
-    mysqli_stmt_execute($stmt);
-    // Confirmamos la consulta y cerramos la conexion
-    mysqli_commit($conn);
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
 
-    header('Location: ../tabla.php');
-} catch (Exception $e) {
+    // Verificar si el email ya existe
+    $stmtEmailCheck = mysqli_stmt_init($conn);
+    $sqlEmailCheck = "SELECT id FROM tbl_alumnos WHERE email = ?";
+    mysqli_stmt_prepare($stmtEmailCheck, $sqlEmailCheck);
+    mysqli_stmt_bind_param($stmtEmailCheck, "s", $email);
+    mysqli_stmt_execute($stmtEmailCheck);
+    mysqli_stmt_store_result($stmtEmailCheck);
+
+    if (mysqli_stmt_num_rows($stmtEmailCheck) > 0) {
+        // Si ya existe un registro con el mismo email, manejar la situación aquí (por ejemplo, mostrar un mensaje de error).
+        $errores .= "?emailExiste=true";
+    }
+
+    mysqli_stmt_close($stmtEmailCheck);
+
+    // Verificar si el teléfono ya existe
+    $stmtPhoneCheck = mysqli_stmt_init($conn);
+    $sqlPhoneCheck = "SELECT id FROM tbl_alumnos WHERE telefono = ?";
+    mysqli_stmt_prepare($stmtPhoneCheck, $sqlPhoneCheck);
+    mysqli_stmt_bind_param($stmtPhoneCheck, "s", $telefono);
+    mysqli_stmt_execute($stmtPhoneCheck);
+    mysqli_stmt_store_result($stmtPhoneCheck);
+
+    if (mysqli_stmt_num_rows($stmtPhoneCheck) > 0) {
+        // Si ya existe un registro con el mismo teléfono, manejar la situación aquí (por ejemplo, mostrar un mensaje de error).
+        $errores .= "&telExiste=true";
+    }
+
+    mysqli_stmt_close($stmtPhoneCheck);
+
+    if ($errores != "") 
+    {
+        $datosRecibidos = array('email' => $email, 'telefono' => $telefono);
+
+        $datosDevueltos = http_build_query(array_map('urlencode', $datosRecibidos));
+
+        header('Location: ../crear.php'.$errores.'&'.$datosDevueltos);
+        die();
+    }
+
+    else 
+    {        
+        // Inserción de datos
+        $stmtInsert = mysqli_stmt_init($conn);
+        $sqlInsert = "INSERT INTO `tbl_alumnos` (`id`, `nombre`, `apellido`, `email`, `pass`, `telefono`, `id_curso`) VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+        mysqli_stmt_prepare($stmtInsert, $sqlInsert);
+        mysqli_stmt_bind_param($stmtInsert, "sssssi", $nombre, $apellido, $email, $pwdEncript, $telefono, $curso);
+        mysqli_stmt_execute($stmtInsert);
+
+        // Confirmamos la inserción y cerramos la conexión
+        mysqli_commit($conn);
+        mysqli_stmt_close($stmtInsert);
+        mysqli_close($conn);
+
+        header('Location: ../tabla.php');
+    } 
+
+}
+
+catch (Exception $e) 
+{
     // Deshacemos las inserciones en el caso de que se genere alguna excepción
     echo "Error: " . $e->getMessage() . "";
     die();
 }
+?>
--- view/tabla.php
+++ view/tabla.php
@@ -1,16 +1,64 @@
 <?php
 include_once("./procesos/conexion.php");
-// Consulta para mostar la tabla de los alumnos
+
+// Configuración de la paginación
+$resultadosPorPagina = 10;
+
+// Obtener la página actual
+$paginaActual = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$inicio = ($paginaActual - 1) * $resultadosPorPagina;
+
+// Obtener el término de búsqueda si se ha enviado
+$nombreBusqueda = isset($_GET['nombre']) ? $_GET['nombre'] : '';
+
+// Consulta para mostrar la tabla de los alumnos con paginación y filtro por nombre
 $sql = "SELECT E.*, D.nombre as 'curso' FROM tbl_alumnos E 
-        INNER JOIN tbl_cursos D ON E.id_curso = D.id ORDER BY id ASC;";
+        INNER JOIN tbl_cursos D ON E.id_curso = D.id 
+        WHERE E.nombre LIKE ? 
+        ORDER BY id ASC LIMIT ?, ?";
 $stmt = mysqli_prepare($conn, $sql);
+
+// Añadir el signo de porcentaje solo si se ha proporcionado un término de búsqueda
+if (!empty($nombreBusqueda)) {
+    $nombreBusqueda =  $nombreBusqueda ;
+    mysqli_stmt_bind_param($stmt, "sii", $nombreBusqueda, $inicio, $resultadosPorPagina);
+} else {
+    // Si no hay término de búsqueda, usar un LIKE que coincida con todo
+    $nombreBusqueda = '%';
+    mysqli_stmt_bind_param($stmt, "sii", $nombreBusqueda, $inicio, $resultadosPorPagina);
+}
+
 mysqli_stmt_execute($stmt);
+
 // Guardamos los datos de la consulta
 $alumnos = mysqli_stmt_get_result($stmt);
+
 // Cerramos la consulta
 mysqli_stmt_close($stmt);
+
+// Calcular el número total de páginas
+$sqlTotal = "SELECT COUNT(*) as total FROM tbl_alumnos E 
+             INNER JOIN tbl_cursos D ON E.id_curso = D.id 
+             WHERE E.nombre LIKE ?";
+$stmtTotal = mysqli_prepare($conn, $sqlTotal);
+
+// Añadir el signo de porcentaje solo si se ha proporcionado un término de búsqueda
+if (!empty($nombreBusqueda)) {
+    $nombreBusquedaTotal = $nombreBusqueda ;
+    mysqli_stmt_bind_param($stmtTotal, "s", $nombreBusquedaTotal);
+} else {
+    // Si no hay término de búsqueda, usar un LIKE que coincida con todo
+    $nombreBusquedaTotal = '%';
+    mysqli_stmt_bind_param($stmtTotal, "s", $nombreBusquedaTotal);
+}
+
+mysqli_stmt_execute($stmtTotal);
+$totalAlumnos = mysqli_fetch_assoc(mysqli_stmt_get_result($stmtTotal))['total'];
+$totalPaginas = ceil($totalAlumnos / $resultadosPorPagina);
+mysqli_stmt_close($stmtTotal);
 mysqli_close($conn);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,6 +70,17 @@ mysqli_close($conn);
 
 <body>
     <a href="./crear.php">Crear alumno</a>
+
+    <!-- Formulario de búsqueda por nombre -->
+    <form action="" method="get">
+        <label for="nombre">Buscar por nombre:</label>
+        <input type="text" id="nombre" name="nombre" value="<?php echo $nombreBusqueda; ?>">
+        <button type="submit">Buscar</button>
+    </form>
+
+    <!-- Botón para eliminar filtros -->
+    <a href="?pagina=1">Eliminar Filtros</a>
+
     <table border=1>
         <tr>
             <th>nombre y apellidos</th>
@@ -41,13 +100,20 @@ mysqli_close($conn);
               <td> {$alumno["pass"]} </td>
               <td> {$alumno["telefono"]} </td>
               <td> {$alumno["curso"]} </td>
-              <td><a href='./notas.php?id={$alumno['id']}'>notas</a>
-              <td><a href='./editar_alumno.php?id={$alumno['id']}'>editar</a>
-              <td><a href='./procesos/eliminar.php?id={$alumno['id']}'>aliminar</a>
+              <td><a href='./notas.php?id={$alumno['id']}'>notas</a></td>
+              <td><a href='./editar_alumno.php?id={$alumno['id']}'>editar</a></td>
+              <td><a href='./procesos/eliminar.php?id={$alumno['id']}'>eliminar</a></td>
               </tr>";
         }
         ?>
     </table>
+
+    <div>
+        <!-- Agregar enlaces de paginación -->
+        <?php for ($i = 1; $i <= $totalPaginas; $i++) : ?>
+            <a href="?pagina=<?php echo $i; ?>&nombre=<?php echo $nombreBusqueda; ?>"><?php echo $i; ?></a>
+        <?php endfor; ?>
+    </div>
 </body>
 
 </html>
