07f6b1e55a12c7e5e631a7f02810c774827e5ae4
Óscar <oscarlm27@gmail.com>
2023-11-14 19:24:14 +0100

    Update

--- view/admin.php
+++ view/admin.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Control</title>
+    <title>Panel de control</title>
 </head>
 <body>
-    <h1>Pantalla profesor</h1>
+    <h1>Administrador</h1>
 </body>
 </html>
